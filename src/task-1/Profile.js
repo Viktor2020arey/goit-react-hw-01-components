e7d8298ea613c84859d4c962048a61ac0576dbbd
Holovatiuk Victor <Stethem696@gmail.com>
2021-06-14 17:59:08 +0300

    закончил оформление первой задачи

--- src/task-1/Profile.js
+++ src/task-1/Profile.js
@@ -1,31 +1,27 @@
 import React from 'react';
+import styles from './Profile.module.css';
 
 const Profile = ({ name, tag, location, avatar, stats }) => (
-  <div className="profile">
-    <div className="description">
-      <img
-        src={avatar}
-        alt="Аватар пользователя"
-        className="avatar"
-        width="120"
-      />
-      <p className="name">{name}</p>
-      <p className="tag">@{tag}</p>
-      <p className="location">{location}</p>
+  <div className={styles.Profile}>
+    <div className={styles.description}>
+      <img src={avatar} alt="Аватар пользователя" className={styles.avatar} />
+      <p className={styles.name}>{name}</p>
+      <p className={styles.tag}>@{tag}</p>
+      <p className={styles.location}>{location}</p>
     </div>
 
-    <ul className="stats">
-      <li>
-        <span className="label">Followers</span>
-        <span className="quantity">{stats.followers}</span>
+    <ul className={styles.stats}>
+      <li className={styles.item}>
+        <span className={styles.label}>Followers</span>
+        <span className={styles.quantity}>{stats.followers}</span>
       </li>
-      <li>
-        <span className="label">Views</span>
-        <span className="quantity">{stats.views}</span>
+      <li className={styles.item}>
+        <span className={styles.label}>Views</span>
+        <span className={styles.quantity}>{stats.views}</span>
       </li>
-      <li>
-        <span className="label">Likes</span>
-        <span className="quantity">{stats.likes}</span>
+      <li className={styles.item}>
+        <span className={styles.label}>Likes</span>
+        <span className={styles.quantity}>{stats.likes}</span>
       </li>
     </ul>
   </div>
